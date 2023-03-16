2f642825c153a03fd91495c5d222325bf6399ebe
pychangeric <pychangeric@gmail.com>
2023-03-16 08:18:33 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(Array) {
+    return ['Antonia', 'Nuru']
+
+}
+
+function returnLastTwoDrivers(Array){
+    return ['Amari', 'Mo']
+}
+
+const selectingDrivers=[returnFirstTwoDrivers,returnLastTwoDrivers]
+
+ const fareMultiplier=createFareMultiplier
+ function createFareMultiplier(createFareMultiplier){
+    return fareMultiplier
+ }
+
+ function createFareMultiplier(num1){
+    return function(num1){
+        return num1*num1;
+    };}
+     const fareDoubler=(num1)=>num1*2;
+     const fareTripler=(num1)=>num1*3;
+
+     function selectDifferentDrivers(arrayOfDrivers){
+        return ['Antonia', 'Nuru']; function selectDifferentDrivers(arrayOfDrivers){
+            return ['Amari', 'Mo']
+        }
+     }
